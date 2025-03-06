1cf745ff4446f4f286f427d9a1673ae200aac21b
Steven de Valk <steven.devalk@atos.net>
2025-03-06 11:12:16 +0100

    Implemented work-item-display component in app-component;

--- ToDo.Client/src/app/components/app.component.ts
+++ ToDo.Client/src/app/components/app.component.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { WorkItem } from "../../models/WorkItem";
+import { WorkItemDisplayComponent } from "../components/work-item-display/work-item-display.component"
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [ WorkItemDisplayComponent ]
 })
 export class AppComponent implements OnInit {
   public workItems: WorkItem[] = [];
--- ToDo.Client/src/app/components/work-item-display/work-item-display.component.ts
+++ ToDo.Client/src/app/components/work-item-display/work-item-display.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { WorkItem } from '../../../models/WorkItem';
 
 @Component({
   selector: 'app-work-item-display',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './work-item-display.component.css'
 })
 export class WorkItemDisplayComponent {
-
+  workItem = input<WorkItem>();
 }
