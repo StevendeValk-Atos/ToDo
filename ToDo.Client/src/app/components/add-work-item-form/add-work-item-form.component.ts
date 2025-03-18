e1ec64ba8fd2fd6f42ae2aef6f11885458938467
Steven de Valk <steven.devalk@atos.net>
2025-03-18 10:25:45 +0100

    Added functionality code to AddWorkItemFormComponent (largely copied from app.component.ts);

--- ToDo.Client/src/app/components/add-work-item-form/add-work-item-form.component.ts
+++ ToDo.Client/src/app/components/add-work-item-form/add-work-item-form.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { WorkItemService } from '../../work-item.service';
 
 @Component({
   selector: 'app-add-work-item-form',
   templateUrl: './add-work-item-form.component.html',
   styleUrls: ['./add-work-item-form.component.css']
 })
-export class AddWorkItemFormComponent implements OnInit {
+export class AddWorkItemFormComponent {
+  private workItemService = inject(WorkItemService);
 
-  constructor() { }
+  public workItemDescription: string = "";
 
-  ngOnInit() {
+  public addWorkItem() {
+    this.workItemService.addWorkItem(this.workItemDescription);
+    this.workItemDescription = "";
   }
 
 }
