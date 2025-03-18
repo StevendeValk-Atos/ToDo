38898e0bfd97f81ee72d9a6e5523de2cb4874e45
Steven de Valk <steven.devalk@atos.net>
2025-03-18 10:03:35 +0100

    Added base for AddWorkItemFormComponent; Added import for AddWorkItemFormComponent in app.module.ts;

---
+++ ToDo.Client/src/app/components/add-work-item-form/add-work-item-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-work-item-form',
+  templateUrl: './add-work-item-form.component.html',
+  styleUrls: ['./add-work-item-form.component.css']
+})
+export class AddWorkItemFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- todo.client/src/app/app.module.ts
+++ todo.client/src/app/app.module.ts
@@ -5,11 +5,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app.component';
+
+import { AddWorkItemFormComponent } from './components/add-work-item-form/add-work-item-form.component';
 import { WorkItemDisplayComponent } from './components/work-item-display/work-item-display.component';
 import { WorkItemListComponent } from "./components/work-item-list/work-item-list.component"
 @NgModule({
   declarations: [
     AppComponent,
+    AddWorkItemFormComponent,
     WorkItemDisplayComponent,
     WorkItemListComponent
   ],
