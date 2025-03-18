bd9076d4460747fff2d1c2509a8c782a6f99cc59
Steven de Valk <steven.devalk@atos.net>
2025-03-18 10:34:33 +0100

    Cleaned up unused functionality code from AppComponent;

--- ToDo.Client/src/app/components/app.component.ts
+++ ToDo.Client/src/app/components/app.component.ts
@@ -1,26 +1,8 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { WorkItem } from "../../models/work-item";
-import { WorkItemService} from "../work-item.service"
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  public workItemService = inject(WorkItemService)
-
-  public workItems: WorkItem[] = [];
-  public workItemDescription: string = "";
-  
-  ngOnInit() {
-    this.workItemService.workItems$.subscribe((workItems) => {
-      this.workItems = workItems;
-    })
-  }
-
-  public addWorkItem() {
-    this.workItemService.addWorkItem(this.workItemDescription);
-    this.workItemDescription = "";
-  }
-}
+export class AppComponent { }
