ce93bc67311bca8d1616a8468840b45172ac0056
Steven de Valk <steven.devalk@atos.net>
2025-03-20 09:41:18 +0100

    Updated generated Storybook documentation; Added WorkItemService provider in WorkItemDisplay Storybook; Added deleteWorkItem and updateWorkItem stubs to WorkItemDisplayComponent Storybook;

--- ToDo.Client/src/app/components/work-item-display/work-item-display.stories.ts
+++ ToDo.Client/src/app/components/work-item-display/work-item-display.stories.ts
@@ -1,16 +1,17 @@
 import { Meta, StoryObj, moduleMetadata } from "@storybook/angular";
+import { HttpClientModule } from "@angular/common/http";
 import { fn } from "@storybook/test";
 
-import { WorkItemDisplayComponent } from "./work-item-display.component";
-import { HttpClientModule } from "@angular/common/http"
 import { WorkItemService } from "../../work-item.service";
+import { WorkItemDisplayComponent } from "./work-item-display.component";
+
 const meta: Meta<WorkItemDisplayComponent> = {
     title: "work-item-display",
     component: WorkItemDisplayComponent,
     decorators: [
         moduleMetadata({
             imports: [HttpClientModule],
-
+            providers: [WorkItemService]
         })
     ]
 }
@@ -18,11 +19,13 @@ const meta: Meta<WorkItemDisplayComponent> = {
 export default meta;
 type Story = StoryObj<WorkItemDisplayComponent>;
 export const Default: Story = {
-    args: {
-        workItem: {
-            id: 1,
-            description: "Prepare presentation",
-            isDone: false
-        }
+  args: {
+    deleteWorkItem: fn(),
+    updateWorkItem: fn(),
+    workItem: {
+        id: 1,
+        description: "Prepare presentation",
+        isDone: false
     }
+}
 }
