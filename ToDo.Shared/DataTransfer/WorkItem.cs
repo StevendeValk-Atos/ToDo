7ae9f12aaf14ef7a6aecdd82a1e0c622675962a0
Steven de Valk <steven.devalk@atos.net>
2025-03-24 14:36:52 +0100

    Added DataTransfer.WorkItem class base;

---
+++ ToDo.Shared/DataTransfer/WorkItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Shared.DataTransfer
+{
+    public class WorkItem
+    {
+    }
+}
