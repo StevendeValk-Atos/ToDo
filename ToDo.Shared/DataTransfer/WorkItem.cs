727d348343115ece7e1b52723c2987cc5076361f
Steven de Valk <steven.devalk@atos.net>
2025-03-24 14:42:28 +0100

    Added properties to DataTransfer.WorkItem;

--- ToDo.Shared/DataTransfer/WorkItem.cs
+++ ToDo.Shared/DataTransfer/WorkItem.cs
@@ -8,5 +8,9 @@ namespace ToDo.Shared.DataTransfer
 {
     public class WorkItem
     {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+        public bool IsMine { get; set; }
     }
 }
