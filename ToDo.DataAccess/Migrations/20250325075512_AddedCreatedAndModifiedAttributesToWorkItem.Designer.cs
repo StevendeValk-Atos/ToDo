e5ce80fbd2cc4833d797b19a46e9d5ba35b3139c
Steven de Valk <steven.devalk@atos.net>
2025-03-25 09:11:28 +0100

    Added 'AddCreatedAndModifiedAttributesToWorkItem' Migration;

--- ToDo.DataAccess/Migrations/20250325075512_AddedCreatedAndModifiedAttributesToWorkItem.Designer.cs
+++ ToDo.DataAccess/Migrations/20250325075512_AddedCreatedAndModifiedAttributesToWorkItem.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToDo.DataAccess;
 
@@ -11,9 +12,11 @@ using ToDo.DataAccess;
 namespace ToDo.DataAccess.Migrations
 {
     [DbContext(typeof(ToDoContext))]
-    partial class ToDoContextModelSnapshot : ModelSnapshot
+    [Migration("20250325075512_AddedCreatedAndModifiedAttributesToWorkItem")]
+    partial class AddedCreatedAndModifiedAttributesToWorkItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ToDo.DataAccess/Migrations/20250325075512_AddedCreatedAndModifiedAttributesToWorkItem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCreatedAndModifiedAttributesToWorkItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "WorkItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "WorkItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "WorkItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "WorkItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "WorkItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "WorkItems");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "WorkItems");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "WorkItems");
+        }
+    }
+}
