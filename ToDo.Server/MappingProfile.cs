7587d26afa00defea9c815dd3d0d6b4c98630d34
Steven de Valk <steven.devalk@atos.net>
2025-03-26 14:29:49 +0100

    Added empty string default values for CreatedBy and ModifiedBy attributes when mapping Shared.DataTransfer.WorkItem -> Shared.Entities.WorkItem;

--- ToDo.Server/MappingProfile.cs
+++ ToDo.Server/MappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using ToDo.Shared.DataTransfer;
+using ToDo.Shared;
 
 namespace ToDo.Server
 {
@@ -9,9 +10,12 @@ namespace ToDo.Server
         {
             CreateMap<Shared.Entities.WorkItem, Shared.DataTransfer.WorkItem>()
                 .ForMember(
-                dest => dest.IsMine,
-                opt => opt.MapFrom(src => src.CreatedBy == "Steven"));
-            CreateMap<Shared.DataTransfer.WorkItem, Shared.Entities.WorkItem>();
+                    dest => dest.IsMine,
+                    opt => opt.MapFrom(src => src.CreatedBy == "Steven")
+                );
+            CreateMap<Shared.DataTransfer.WorkItem, Shared.Entities.WorkItem>()
+                .ForMember( dest => dest.CreatedBy, opt => opt.MapFrom(src => ""))
+                .ForMember( dest => dest.ModifiedBy, opt => opt.MapFrom(src => ""));
         }
     }
 }
