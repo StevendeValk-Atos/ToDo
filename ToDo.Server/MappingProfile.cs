466249771e49142a2edaf54989e260e95cdacfed
Steven de Valk <steven.devalk@atos.net>
2025-03-26 10:58:02 +0100

    Added Mappers for Shared.Entities.WorkItem -> Shared.DataTransfer.WorkItem and Shared.DataTransfer.WorkItm -> Shared.Entities.WorkItem;

--- ToDo.Server/MappingProfile.cs
+++ ToDo.Server/MappingProfile.cs
@@ -1,9 +1,17 @@
 ﻿using AutoMapper;
+using ToDo.Shared.DataTransfer;
 
 namespace ToDo.Server
 {
     public class MappingProfile : Profile
     {
-
+        public MappingProfile()
+        {
+            CreateMap<Shared.Entities.WorkItem, Shared.DataTransfer.WorkItem>()
+                .ForMember(
+                dest => dest.IsMine,
+                opt => opt.MapFrom(src => src.CreatedBy == "Steven" ? true : false));
+            CreateMap<Shared.Entities.WorkItem, WorkItem>().ReverseMap();
+        }
     }
 }
