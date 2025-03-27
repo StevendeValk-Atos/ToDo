38d21a1495581d9c0105cf0d7511f418cc7505e6
Steven de Valk <steven.devalk@atos.net>
2025-03-27 09:38:30 +0100

    Fixed workItems dummy data format in WorkItemListComponent.stories; Automatic updates done in Storybook documentation.json;

--- ToDo.Client/src/app/components/work-item-list/work-item-list.stories.ts
+++ ToDo.Client/src/app/components/work-item-list/work-item-list.stories.ts
@@ -31,16 +31,66 @@ type Story = StoryObj<WorkItemListComponent>;
 export const Default: Story = {
     args: {
         workItems: [
-                { id: 1, description: "Design a new logo for the company", isDone: false },
-                { id: 2, description: "Update the company website with new content", isDone: true },
-                { id: 3, description: "Create a marketing plan for the new product launch", isDone: false },
-                { id: 4, description: "Write a user manual for the new software release", isDone: true },
-                { id: 5, description: "Test the new feature in the software application", isDone: false },
-                { id: 6, description: "Create training materials for the new software release", isDone: true },
-                { id: 7, description: "Research new technology for use in the company's products", isDone: false },
-                { id: 8, description: "Brainstorm and develop a new product concept for the company", isDone: true },
-                { id: 9, description: "Develop a social media campaign to promote the company's products", isDone: false },
-                { id: 10, description: "Develop a budget plan for the upcoming quarter", isDone: true },
+            {
+                id: 1,
+                description: "Design a new logo for the company",
+                isDone: false,
+                isMine: true
+            },
+            {
+              id: 2,
+              description: "Update the company website with new content",
+              isDone: true,
+              isMine: false
+            },
+            {
+              id: 3,
+              description: "Create a marketing plan for the new product launch",
+              isDone: false,
+              isMine: false
+            },
+            {
+              id: 4,
+              description: "Write a user manual for the new software release",
+              isDone: true,
+              isMine: true
+            },
+            {
+              id: 5,
+              description: "Test the new feature in the software application",
+              isDone: false,
+              isMine: false
+            },
+            {
+              id: 6,
+              description: "Create training materials for the new software release",
+              isDone: true,
+              isMine: false
+            },
+            {
+              id: 7,
+              description: "Research new technology for use in the company's products",
+              isDone: false,
+              isMine: false
+            },
+            {
+              id: 8,
+              description: "Brainstorm and develop a new product concept for the company",
+              isDone: true,
+              isMine: false
+            },
+            {
+              id: 9,
+              description: "Develop a social media campaign to promote the company's products",
+              isDone: false,
+              isMine: false
+            },
+            {
+              id: 10,
+              description: "Develop a budget plan for the upcoming quarter",
+              isDone: false,
+              isMine: false
+            },
         ]
     }
 }
