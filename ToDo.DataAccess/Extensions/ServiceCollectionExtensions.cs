4e0c7e094c8145e321d804c6f0eb617011e9df67
Steven de Valk <steven.devalk@atos.net>
2025-03-28 10:05:08 +0100

    Added ServiceCollectionExtensions class to ToDo.DataAccess; Added ToDo.DataAccess.Extensions.ServiceCollectionExtensions.AddDatabase method to handle craetion of ToDoConext and ToDoContext provider;

---
+++ ToDo.DataAccess/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.DataAccess.Extensions
+{
+    public static class ServiceCollectionExtensions 
+    {
+        public static IServiceCollection AddDatabase(
+            this IServiceCollection services,
+            string connectionString
+        )
+        {
+            services.AddDbContext<ToDoContext>(options =>
+            {
+                options.UseSqlServer(
+                    connectionString,
+                    sqlOptions => sqlOptions.MigrationsAssembly("ToDo.DataAccess")
+                );
+            });
+            services.AddScoped<Func<ToDoContext>>((provider) => () =>
+            {
+                return provider.GetService<ToDoContext>()!;
+            });
+
+            return services;
+        }
+    }
+}
