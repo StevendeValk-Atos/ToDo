8805ad906e85117f59dccea874c263d6a3b8dc76
Steven de Valk <steven.devalk@atos.net>
2025-03-28 11:08:51 +0100

    Added base for SeedDatabase method in ToDo.DataAccess.Extensions;

--- ToDo.DataAccess/Extensions/ApplicationBuilderExtensions.cs
+++ ToDo.DataAccess/Extensions/ApplicationBuilderExtensions.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 
 namespace ToDo.DataAccess.Extensions
 {
     public static class ApplicationBuilderExtensions
     {
+        public static IApplicationBuilder SeedDatabase(this IApplicationBuilder builder)
+        {
+            return builder;
+        }
     }
 }
