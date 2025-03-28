81e4802b63911f1923a4c1ca65b670dfa89efc30
Steven de Valk <steven.devalk@atos.net>
2025-03-28 10:41:48 +0100

    Added AddMappers method to have a dedicated method to configure mapper profiles in ToDo.Server.Extensions.ServiceCollectionExtensions;

--- ToDo.Server/Extensions/ServiceCollectionExtensions.cs
+++ ToDo.Server/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,20 @@
-﻿namespace ToDo.Server.Extensions
+﻿using AutoMapper;
+
+namespace ToDo.Server.Extensions
 {
-    public class ServiceCollectionExtensions
+    public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddMappers(this IServiceCollection services)
+        {
+            var mapperConfig = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new MappingProfile());
+            });
+            IMapper mapper = mapperConfig.CreateMapper();
+
+            services.AddSingleton<IMapper>(mapper);
+
+            return services;
+        }
     }
 }
